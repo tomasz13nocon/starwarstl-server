ff9a288056d4921ba9f443c6471f837cef2eec9b
Tomasz Nocoń <tomasz13nocon@gmail.com>
2023-01-16 12:11:30 +0100

    region change

--- s3Image.js
+++ s3Image.js
@@ -6,7 +6,7 @@ import { S3Client, GetObjectCommand, PutObjectCommand, DeleteObjectCommand, Head
 const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 const BUCKET = "starwarstl";
-const s3client = new S3Client({ region: "us-east-2", credentials: { accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY } });
+const s3client = new S3Client({ region: "us-east-1", credentials: { accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY } });
 
 export class S3Image {
   constructor(filename) {
